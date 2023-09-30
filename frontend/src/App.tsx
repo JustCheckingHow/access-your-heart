3ba21b41618986959a0fdca504bb1758e36764e7
DominikaKulig <dku@spyro-soft.com>
2023-09-30 21:48:12 +0200

    Added main search page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,8 @@ import { Checkbox } from "./components/ui/checkbox";
 import { Alert, AlertDescription, AlertTitle } from "./components/ui/alert";
 import { Info, Plus } from "lucide-react";
 import { Badge } from "./components/ui/badge";
+import SearchPage from "./components/ui/SearchPage";
+
 
 interface SkillsResponse {
   skills: Option[];
@@ -20,6 +22,7 @@ interface HobbiesResponse {
 }
 
 function App() {
+
   const [skillsOptions, setSkillsOptions] = useState<SkillsResponse["skills"]>(
     []
   );
@@ -104,115 +107,7 @@ function App() {
 
   return (
     <div className="container mx-auto">
-      <h3 className="scroll-m-20 text-4xl font-semibold tracking-tight text-center">
-        Cześć,
-      </h3>
-      <h4 className="scroll-m-20 text-2xl font-semibold tracking-tight text-center">
-        Zaplanuj z nami swoją karierę!
-      </h4>
-      <div className="grid w-full max-w-md items-center gap-4 mt-6 mx-auto">
-        <Label className="text-md">
-          1. Jakie umiejętności chcesz rozwijać?
-        </Label>
-        <FancyMultiSelect options={skillsOptions} />
-      </div>
-      <div className="grid w-full max-w-md items-center gap-4 mt-6 mx-auto">
-        <Label className="text-md">
-          2. W jakim zawodzie się widzisz w przyszłości?
-        </Label>
-        <FancyMultiSelect options={professionsOptions} />
-      </div>
-      <div className="grid w-full max-w-md items-center gap-4 mt-6 mx-auto">
-        <Label className="text-md">
-          3. Czy masz już zawód? Na podstawie umiejetnosci wymaganych w tym
-          zawodzie uzupelnimy wyszukiwania
-        </Label>
-        <div className="flex justify-between space-x-2">
-          <div className="flex items-center space-x-2">
-            <Checkbox id="profession-yes" />
-            <label
-              htmlFor="terms"
-              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-            >
-              Tak, mam zawód
-            </label>
-          </div>
-          <div className="flex items-center space-x-2">
-            <Checkbox id="profession-no" />
-            <label
-              htmlFor="terms"
-              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-            >
-              Nie, nie mam zawodu
-            </label>
-          </div>
-        </div>
-      </div>
-      <div className="grid w-full max-w-md items-center gap-4 mt-6 mx-auto">
-        <Label className="text-md">
-          4. Jakie hobby chcialbys wykorzystywac w swoim zawodzie?
-        </Label>
-        <div className="flex items-center space-x-2">
-          <Checkbox id="hobby-none" />
-          <label
-            htmlFor="terms"
-            className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-          >
-            Żadne
-          </label>
-        </div>
-        <FancyMultiSelect options={hobbies} />
-      </div>
-      <Alert className="max-w-lg mx-auto mt-2">
-        <Info className="h-4 w-4" />
-        <AlertTitle className="space-y-1 leading-6">
-          Te zawody pasują do Twoich elementów wyszukiwania. Czy chcesz je dodać
-          do możliwych zawodów?
-        </AlertTitle>
-        <AlertDescription>
-          <div className="flex flex-wrap">
-            {professionForHobbies.map((profession) => (
-              <Badge variant="secondary" className="mx-1 my-1">
-                <button className="mx-1 ring-offset-background rounded-full outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2">
-                  <Plus className="h-3 w-3 text-muted-foreground hover:text-foreground" />
-                </button>
-                {profession.name}
-              </Badge>
-            ))}
-          </div>
-        </AlertDescription>
-      </Alert>
-      <div className="grid w-full max-w-md items-center gap-4 mt-6 mx-auto">
-        <Label className="text-md">
-          5. Ile czasu mozesz poswiecic na naukę?
-        </Label>
-        <div className="flex justify-between flex-wrap">
-          {[
-            "Nie mam ograniczneń",
-            "Wieczorowy",
-            "Weekendy",
-            "20h tygodniowo",
-            "10h co drugi tydzien",
-            "Bloker",
-          ].map((option) => (
-            <div className="flex items-center space-x-2 my-1" key={option}>
-              <Checkbox id="option" />
-              <label
-                htmlFor="terms"
-                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-              >
-                {option}
-              </label>
-            </div>
-          ))}
-        </div>
-      </div>
-      <div className="grid w-full max-w-md items-center gap-4 mt-6 mx-auto">
-        <Label className="text-md">
-          6. W których miastach możesz studiować?
-        </Label>
-        <FancyMultiSelect options={professionsOptions} />
-      </div>
+      <SearchPage />
     </div>
   );
 }
